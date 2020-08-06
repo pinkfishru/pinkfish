63b497d9f9b16fa6aead499ea29f6332905b6bdb
Boroda29 <as.ivanov29@yandex.ru>
2020-08-07 00:16:03 +0300

    ver_0.0.2 НУ начнем с этой ветки разработку

--- pinkfish/settings.py
+++ pinkfish/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gto8bmxg#nzgjzk_l=^n27t0413reiap$y2eea9msmtw=r_#(t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
